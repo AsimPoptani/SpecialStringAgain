0bd7edacdc7c1eb0c5e68e6d4691e59fb4447f65
Superev12 <evan.snoswell@gmail.com>
2019-11-05 18:42:50 +0000

    Wrote recursive function with dynamic programming

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import collections
+
+s_set = collections.defaultdict(int)
+
+
 def substring_checker(in_str):
     # All same?
     if len(set(in_str)) == 1:
+        print("found special", in_str)
         return True
         
     # All same except middle?
@@ -9,6 +15,7 @@ def substring_checker(in_str):
         if len(in_list) % 2 == 1:
             del in_list[len(in_list) // 2]
             if len(set(in_list)) == 1:
+                print("found special", in_str)
                 return True
             else:
                 return False
@@ -16,16 +23,21 @@ def substring_checker(in_str):
             return False
 
 def calc_substring(string):
-    total=0
-    for index in range(1,len(string)+1):
-        head=index
-        tail=0
-        while (head != len(string)+1):
-            if substring_checker(string[tail:head]): # insert function here
-                total+=1
-            tail+=1
-            head+=1
-    return total
+    if len(string) == 1:
+        return 1
+    elif string in s_set.keys():
+        return s_set[string]
+    else:
+        # Not in, calculate
+        if substring_checker(string):
+            value = 1 + calc_substring(string[0:len(string)-1]) + calc_substring(string[1:len(string)])
+            s_set[string] = value
+            return value
+        else:
+            value = calc_substring(string[0:len(string)-1]) + calc_substring(string[1:len(string)])
+            s_set[string] = value
+            return value
 
-exampleString ="abcbaba"
+
+exampleString ="mnonopoo"
 print(calc_substring(exampleString))
